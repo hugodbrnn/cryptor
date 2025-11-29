fd73fd65de2e32e6ddbc1919898cc36157736afe
hugodbrnn <hugo.deberne@edu.devinci.fr>
2025-11-29 23:12:03 +0100

    update5

--- cryptor/src/main.rs
+++ cryptor/src/main.rs
@@ -3,8 +3,8 @@ mod io;
 mod crypto;
 
 fn main() -> anyhow::Result<()> {
-    let args = cli::parse_args();
-    cli::run(args)?;
+    let args = cli::parse_args();   // Clap robust
+    cli::run(args)?;                 // exécution
 
-    Ok(())
+    Ok(()) // Tout s'est bien passé
 }
